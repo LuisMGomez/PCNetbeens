295532e5c154c2b75474f536bcb3e411b6acd698
Luis Manuel <Luis Manuel@LAPTOP-QIBFEIPR>
2019-11-09 19:21:39 +0100

    Rama cambio mezclada con la rama master

---
+++ src/luismanuelgomezadivino/Aciertos.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luismanuelgomezadivino;
+/**
+ *
+ * @author Luis Manuel Gomez
+ */
+    
+import java.util.Scanner;
+
+public class Aciertos {
+
+    public static void main(String[] args) {
+   
+        int n = 0,cont = 0;
+        int x = (int) (41 * Math.random())+10; // El numero a adivinar esta entre 10 y 50 ambos inclusive
+        //System.out.print(x);           
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\nIntroduce el valor de X: ");
+            n = sc.nextInt();
+            if (n > x) {
+                System.out.print("Te pasaste Amigo");
+            } else if (n < x){
+                System.out.print("Casi cerca ...");
+            }
+            cont++;
+        } while (n != x);
+        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        sc.close();
+    }
+
+    
+}
